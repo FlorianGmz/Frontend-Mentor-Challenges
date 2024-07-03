16abdeec130621748bb0a3555feb801a2527d1eb
FlorianGmz <florian.g.code@gmail.com>
2024-07-03 22:40:53 +0200

    CategoryPicker in progress

--- product-feedback/src/components/Category.tsx
+++ product-feedback/src/components/Category.tsx
@@ -1,7 +1,15 @@
-const Category = () => {
+import React from "react";
+
+interface CategoryProps {
+  categoryName: string;
+}
+
+const Category: React.FC<CategoryProps> = ({ categoryName }) => {
   return (
-    <div className="rounded-xl bg-el_def px-[16px] py-[5px] text-body-3 text-bt-blue_def">
-      Category
+    <div className="cursor-pointer items-start rounded-xl bg-el_def px-[16px] py-[5px] text-body-3 capitalize text-bt-blue_def transition-colors hover:bg-el_hover">
+      {categoryName === "ui" || categoryName === "ux"
+        ? categoryName.toUpperCase()
+        : categoryName}
     </div>
   );
 };
--- product-feedback/src/components/CategoryPicker.tsx
+++ product-feedback/src/components/CategoryPicker.tsx
@@ -1,11 +1,26 @@
 import Category from "./Category";
 
+import data from "../data/data.json";
+import { useState } from "react";
+
 const CategoryPicker = () => {
+  const productRequests = data.productRequests;
+  const allDataCategories = productRequests.map((request) => request.category);
+  const nonDataCategories = ["all", "ui", "ux"];
+  const categories = [...nonDataCategories, ...allDataCategories];
+  const filteredCategories = Array.from(new Set(categories));
+
+  const [categorySelected, setCategorySelected] = useState("all");
+
   return (
-    <div className="flex h-[166px] w-[255px] gap-[8px] rounded-xl bg-bt-white_def p-[24px]">
-      <Category />
-      <Category />
-      <Category />
+    <div className="flex h-[166px] w-[255px] flex-wrap items-start gap-[8px] rounded-xl bg-bt-white_def p-[24px]">
+      {filteredCategories.map((category) => (
+        <Category
+          key={category}
+          categoryName={category}
+          categorySelected={categorySelected}
+        />
+      ))}
     </div>
   );
 };
