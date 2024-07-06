7441094df9c40cadf9eb9ea0489dfc7d48b38ed0
FlorianGmz <florian.g.code@gmail.com>
2024-07-06 10:15:17 +0200

    static CategoryPicker done

--- product-feedback/src/components/Category.tsx
+++ product-feedback/src/components/Category.tsx
@@ -2,16 +2,28 @@ import React from "react";
 
 interface CategoryProps {
   categoryName: string;
-  categorySelected: string;
-  setCategorySelected: React.Dispatch<React.SetStateAction<string>>;
+  selectedCategory: string;
+  setSelectedCategory: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Category: React.FC<CategoryProps> = ({ categoryName }) => {
-  const handleClick = () => {};
+const Category: React.FC<CategoryProps> = ({
+  categoryName,
+  selectedCategory,
+  setSelectedCategory,
+}) => {
+  const handleClick = () => {
+    setSelectedCategory("");
+    setSelectedCategory(categoryName);
+  };
+  console.log(selectedCategory);
+  const isSelected = categoryName === selectedCategory;
+  const bgColor = isSelected ? "bg-el_active" : "bg-el_def";
+  const textColor = isSelected ? "text-bt-white_def" : "text-bt-blue_def";
+  const hoverColor = isSelected ? "" : "hover:bg-el_hover";
   return (
     <div
-      onClick={handleClick}
-      className="cursor-pointer items-start rounded-xl bg-el_def px-[16px] py-[5px] text-body-3 capitalize text-bt-blue_def transition-colors hover:bg-el_hover"
+      onClick={() => handleClick()}
+      className={`cursor-pointer items-start rounded-xl ${bgColor} px-[16px] py-[5px] text-body-3 capitalize ${textColor} transition-colors ${hoverColor}`}
     >
       {categoryName === "ui" || categoryName === "ux"
         ? categoryName.toUpperCase()
--- product-feedback/src/components/CategoryPicker.tsx
+++ product-feedback/src/components/CategoryPicker.tsx
@@ -9,7 +9,7 @@ const CategoryPicker = () => {
   const nonDataCategories = ["all", "ui", "ux"];
   const categories = [...nonDataCategories, ...allDataCategories];
   const filteredCategories = Array.from(new Set(categories));
-  const [categorySelected, setCategorySelected] = useState("all");
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
   return (
     <div className="flex h-[166px] w-[255px] flex-wrap items-start gap-[8px] rounded-xl bg-bt-white_def p-[24px]">
@@ -17,8 +17,8 @@ const CategoryPicker = () => {
         <Category
           key={category}
           categoryName={category}
-          categorySelected={categorySelected}
-          setCategorySelected={setCategorySelected}
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
         />
       ))}
     </div>
