d4b48067dd55b2575b25894eb43d469ca174f364
FlorianGmz <florian.g.code@gmail.com>
2024-07-06 16:53:31 +0200

    feedback filtered by category complete

--- product-feedback/src/components/CategoryPicker/CategoryPicker.tsx
+++ product-feedback/src/components/CategoryPicker/CategoryPicker.tsx
@@ -1,15 +1,23 @@
 import Category from "./Category";
-
 import data from "../../data/data.json";
-import { useState } from "react";
+import React from "react";
 
-const CategoryPicker = () => {
-  const productRequests = data.productRequests;
-  const allDataCategories = productRequests.map((request) => request.category);
+interface CategoryPickerProps {
+  selectedCategory: string;
+  setSelectedCategory: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const CategoryPicker: React.FC<CategoryPickerProps> = ({
+  selectedCategory,
+  setSelectedCategory,
+}) => {
+  const allDataCategories = data.productRequests.map(
+    (request) => request.category,
+  );
   const nonDataCategories = ["all", "ui", "ux"];
   const categories = [...nonDataCategories, ...allDataCategories];
+
   const filteredCategories = Array.from(new Set(categories));
-  const [selectedCategory, setSelectedCategory] = useState("all");
 
   return (
     <div className="flex h-[166px] w-[255px] flex-wrap items-center gap-[8px] rounded-xl bg-bt-white_def p-[24px]">
--- product-feedback/src/components/SuggestionsPage.tsx/SuggestionsPage.tsx
+++ product-feedback/src/components/SuggestionsPage.tsx/SuggestionsPage.tsx
@@ -4,21 +4,34 @@ import RoadmapNav from "../RoadmapNav/RoadmapNav";
 import SuggestionsBar from "../SuggestionsBar/SuggestionsBar";
 import data from "../../data/data.json";
 import Feedback from "../Feedback/Feedback";
+import { useState } from "react";
 
 const SuggestionsPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
   const feedbackSuggestions = data.productRequests.filter((request) => {
     return request.status === "suggestion";
   });
+
+  const filteredSuggestions = feedbackSuggestions.filter((suggestion) =>
+    selectedCategory === "all"
+      ? feedbackSuggestions
+      : suggestion.category === selectedCategory,
+  );
+
   return (
     <div className="mx-auto my-[100px] flex w-[1110px] gap-[30px]">
       <div className="flex flex-col gap-[24px]">
         <FrontendMentorSticker />
-        <CategoryPicker />
+        <CategoryPicker
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <RoadmapNav />
       </div>
       <div className="flex flex-col gap-[20px]">
         <SuggestionsBar suggestionsCount={feedbackSuggestions.length} />
-        {feedbackSuggestions.map((feedback) => (
+        {filteredSuggestions.map((feedback) => (
           <Feedback key={feedback.id} feedback={feedback} />
         ))}
       </div>
