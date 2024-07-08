e3e2295bb5ae39380661c9bc14bd30986d4c3b03
FlorianGmz <florian.g.code@gmail.com>
2024-07-08 16:58:47 +0200

    Link to FeadbackDetail complete + routing

--- product-feedback/src/App.tsx
+++ product-feedback/src/App.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SuggestionsPage from "./components/SuggestionsPage.tsx/SuggestionsPage";
+import FeedbackDetailPage from "./components/FeedbackDetailPage.tsx/FeedbackDetailPage";
 
 const router = createBrowserRouter([
   {
     path: "/suggestions",
     element: <SuggestionsPage />,
+  },
+  {
+    path: "/feedback/:id",
+    element: <FeedbackDetailPage />,
   },
 ]);
 
