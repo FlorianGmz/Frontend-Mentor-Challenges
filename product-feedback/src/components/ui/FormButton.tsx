4fc08385bc43322e79e95dec40496bbc8ca25304
FlorianGmz <florian.g.code@gmail.com>
2024-09-12 10:49:23 +0200

    Button refacto

--- product-feedback/src/components/ui/FormButton.tsx
+++ product-feedback/src/components/ui/FormButton.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 
 interface FormButtonProps {
-  type: "submit" | "reset" | "button" | undefined;
+  type: "submit" | "reset" | "button";
   label: string;
   feedbackId?: string | undefined;
   onClick?: () => void;
 }
 
 const FormButton: React.FC<FormButtonProps> = ({ type, label, onClick }) => {
-  const commentButton = type === "submit";
-  const cancelButton = label === "cancel";
-  const deleteButton = label === "delete";
+  const isCommentButton = type === "submit";
+  const isCancelButton = label.toLocaleLowerCase() === "cancel";
+  const isDeleteButton = label.toLocaleLowerCase() === "delete";
 
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`cursor-pointer ${deleteButton ? "bg-bt-red_def hover:bg-bt-red_hover" : ""} ${cancelButton ? "bg-el-font_def hover:bg-bt-dark-blue_hover" : ""} rounded-xl ${commentButton ? "bg-bt-purple_def active:bg-bt-purple_hover xl:hover:bg-bt-purple_hover" : ""} px-[16px] py-[11px] text-[13px] font-bold capitalize text-el_def transition-colors md:px-[24px] md:py-[12px] md:text-[14px] xl:justify-center`}
+      className={`cursor-pointer ${isDeleteButton ? "bg-bt-red_def hover:bg-bt-red_hover" : ""} ${isCancelButton ? "bg-el-font_def hover:bg-bt-dark-blue_hover" : ""} rounded-xl ${isCommentButton ? "bg-bt-purple_def active:bg-bt-purple_hover xl:hover:bg-bt-purple_hover" : ""} px-[16px] py-[11px] text-[13px] font-bold capitalize text-el_def transition-colors md:px-[24px] md:py-[12px] md:text-[14px] xl:justify-center`}
     >
       {label}
     </button>
